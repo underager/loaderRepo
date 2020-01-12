cf7e172712690a5525a1187c2a2b5b767653c5e5
Atul Nirbhavane <anirbhavane106@gmail.com>
2020-01-12 17:16:48 +0530

    initial commit

---
+++ Sample1/js/indexJs.js
@@ -0,0 +1,6 @@
+ function function1(){
+    console.log('button1 clicked');
+    var section1 = document.getElementById('id1');
+    console.log(section1);
+    section1.classList.toggle('loading_anime');
+}
